5443ce0b499a9c427d2d3e77a14c65dc16a23bfa
jerbi07 <87204525+jerbi07@users.noreply.github.com>
2022-02-15 23:06:29 +0100

    Add files via upload

---
+++ tp-teams-handling-api/tp-teams-handling_api/target/classes/data.sql
@@ -0,0 +1,11 @@
+INSERT INTO team (name, slogan) VALUES ('PSG', 'Revons plus grand');
+INSERT INTO team (name, slogan) VALUES ('Real Madrid', 'Les galactiques');
+INSERT INTO team (name, slogan) VALUES ('Barcelone', 'La Macia');
+INSERT INTO team (name, slogan) VALUES ('Bayern', 'Les puissants en Allemagne');
+INSERT INTO team (name, slogan) VALUES ('Manchester United', 'Les red devils');
+
+
+insert into Player (id, name, number, position,team_id) values(1,'Di Maria','11','Forward',1);
+insert into Player (id, name, number, position,team_id) values(2,'Hazard','7','Forward',2);
+insert into Player (id, name, number, position,team_id) values(3,'Sergio Ramos','4','Defense',3);
+insert into Player (id, name, number, position,team_id) values(4,'Navas','1','Goalkeeper',4);
